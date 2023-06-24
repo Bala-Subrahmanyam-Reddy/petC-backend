2c8f8c5db273b06f0dfda667d838ec263bc6b588
Reddy Bala Subrahmanyam <121880674+Bala-Subrahmanyam-Reddy@users.noreply.github.com>
2023-06-24 23:30:05 +0530

    Update petBreads.py

--- models/petBreads.py
+++ models/petBreads.py
@@ -4,7 +4,7 @@ from config.db import metadata
 
 
 petBreadsModal = Table(
-    'petBreads', metadata,
+    'petbreads', metadata,
     Column('id', String(255), primary_key=True),
     Column('name', String(255)),
 )
