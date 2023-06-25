65f9812933792cb586ed9a3377342b79dd330aab
Reddy Bala Subrahmanyam <121880674+Bala-Subrahmanyam-Reddy@users.noreply.github.com>
2023-06-25 11:44:12 +0530

    Update db.py

--- config/db.py
+++ config/db.py
@@ -1,7 +1,7 @@
 import databases
 import sqlalchemy
 
-DATABASE_URL="mysql+pymysql://sql12628472:1XdxYgaw3G@sql12.freesqldatabase.com:3306/sql12628472"
+DATABASE_URL="sqlite:///./petcdb.db"
 
 database = databases.Database(DATABASE_URL)
 
